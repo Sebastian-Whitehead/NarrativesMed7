d1262acda0fcbee471ef4919ca45cdb769deb3d7
Sebastian W <sebastianmikael2000@gmail.com>
2023-11-22 12:17:46 +0100

    Assingned the correct narration files + modified button script

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -3,56 +3,107 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]  // This line ensures that an AudioSource component is added when this script is added to a GameObject
-
-// Button class that can be attached to a GameObject in Unity
+[RequireComponent(typeof(AudioSource))]
 public class Button : MonoBehaviour
 {
-    //Unity defined Variables
+    [Tooltip("Maximum distance for the button interaction.")]
     public float maxDistance = 5f;
+
+    [Tooltip("List of objects bound to this button.")]
     public List<MonoBehaviour> boundObjects = new List<MonoBehaviour>();
-    public AudioClip buttonSound; // Public AudioClip variable for the sound effect
+
+    [Tooltip("Delay before triggering the final triggers.")]
+    public float delayBeforeFinalTriggers = 0f;
+    
+    [Tooltip("List of final triggers that will run after bound triggers.")]
+    public List<MonoBehaviour> finalTriggers = new List<MonoBehaviour>();
+
+    [Tooltip("Sound played when the button is pressed.")]
+    public AudioClip buttonSound;
+
+    [Tooltip("Key code to trigger the button.")]
     public KeyCode inputKey;
-    
-    //Private Variables
-    private List<ITrigger> _mBoundTriggers = new List<ITrigger>(); // List to hold the triggers bound to this button
-    private AudioSource _audioSource; // Private AudioSource variable
-    
-    
-    
-    // Start is called before the first frame update
+
+    private List<ITrigger> _mBoundTriggers = new List<ITrigger>();
+    private List<ITrigger> _mFinalTriggers = new List<ITrigger>();
+    private AudioSource _audioSource;
+
+    private bool _boundTriggersCompleted = false;
+
     void Start()
     {
-        // Filter boundObjects to only include MonoBehaviours that implement the ITrigger interface
         _mBoundTriggers = boundObjects.OfType<ITrigger>().ToList();
         boundObjects.Clear();
-        
-        _audioSource = GetComponent<AudioSource>(); // Initialize the AudioSource component
+
+        _mFinalTriggers = finalTriggers.OfType<ITrigger>().ToList();
+        finalTriggers.Clear();
+
+        _audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        // Check if a raycast from the camera in the direction of the mouse cursor hits this button and if 'E' key was pressed
-        if (Physics.Raycast(ray, out hit, maxDistance) && hit.transform == this.transform && Input.GetKeyDown(KeyCode.E))
+        if (Physics.Raycast(ray, out hit, maxDistance) && hit.transform == this.transform && Input.GetKeyDown(inputKey))
         {
             _audioSource.PlayOneShot(buttonSound);
+
+            // Start the TriggerBoundObjects coroutine without waiting for completion
             StartCoroutine(TriggerBoundObjects());
         }
     }
-    
+
     IEnumerator TriggerBoundObjects()
     {
+        float totalWaitForCompletionDuration = 0f;
+        bool narrationTriggerEncountered = false;
+
         foreach (var trigger in _mBoundTriggers)
         {
             trigger.Trigger();
 
+            if (trigger.Type == Fx.Narration)
+            {
+                narrationTriggerEncountered = true;
+                // Add the duration of the Narration trigger
+                totalWaitForCompletionDuration += trigger.Duration;
+            }
+
+            if (!narrationTriggerEncountered && trigger.WaitForCompletion)
+            {
+                // If WaitForCompletion is true and narration has not been encountered,
+                // include the duration in the totalWaitForCompletionDuration
+                totalWaitForCompletionDuration += trigger.Duration;
+            }
+
             if (trigger.WaitForCompletion)
             {
                 yield return new WaitForSeconds(trigger.Duration);
+            }
+        }
+
+        // Mark bound triggers as completed
+        _boundTriggersCompleted = true;
+
+        // Check if both bound triggers and final triggers are completed
+        if (_boundTriggersCompleted)
+        {
+            // Calculate the time to wait before triggering final triggers
+            float delay = totalWaitForCompletionDuration + delayBeforeFinalTriggers;
+
+            // Delay before triggering final triggers
+            yield return new WaitForSeconds(delay);
+
+            foreach (var finalTrigger in _mFinalTriggers)
+            {
+                finalTrigger.Trigger();
+
+                if (finalTrigger.WaitForCompletion)
+                {
+                    yield return new WaitForSeconds(finalTrigger.Duration);
+                }
             }
         }
     }
